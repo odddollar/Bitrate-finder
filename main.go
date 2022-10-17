6065214d5b4cbb46a4b054fd8fdbc237f105fcb1
Simon Eason <sie.eason@gmail.com>
2022-10-17 20:31:12 +0800

    Added comments

--- main.go
+++ main.go
@@ -16,70 +16,98 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
+// set global variables for maintaining state
 var outputText string
 var maxB int = 0
 var minB int = 0
 var ignoreZero bool = true
 
 func main() {
+	// create app
 	app := app.New()
 	mainWindow := app.NewWindow("Bitrate Finder")
 
+	// create path entry widget
 	path := widget.NewEntry()
 	path.SetPlaceHolder("Path to videos")
 
+	// create folder selection button widget
 	folderSelect := widget.NewButton("...", func() {
+		// create dialog with callback
 		d := dialog.NewFolderOpen(func(lu fyne.ListableURI, err error) {
+			// check if nothing has been selected
 			if lu == nil {
 				return
 			}
 
+			// get selected items in directory and remove filename from first item
 			URIList, _ := lu.List()
 			j := strings.Split(URIList[0].Path(), "/")
 			formattedText := strings.Join(j[0:len(j)-1], "/")
+
+			// set formatted text in folder field
 			path.SetText(formattedText)
 		}, mainWindow)
+
+		// show folder selection dialog
 		d.Show()
 	})
 
+	// create options button to open options window
 	options := widget.NewButton("Options", func() {
 		showOptions(app)
 	})
 
+	// create output box with minimum number of rows visible
 	outputBox := widget.NewMultiLineEntry()
 	outputBox.SetMinRowsVisible(21)
 
+	// create run button widget
 	run := widget.NewButton("Run", func() {
+		// don't do anything if no path entered
 		if path.Text == "" {
 			return
 		}
 
+		// run as separate thread
 		go func() {
+			// walk through selected directory
 			err := filepath.Walk(path.Text, func(path string, info fs.FileInfo, err error) error {
+				// return errors that occur
 				if err != nil {
 					return err
 				}
 
+				// only run code for files
 				if info.IsDir() {
 					return nil
 				}
 
+				// execute command to find bitrate and handle error
 				output, err := exec.Command("cmd", "/c", "ffprobe", "-v", "error", "-show_entries", "format=bit_rate", "-of", "default=noprint_wrappers=1:nokey=1", path).Output()
 				if err != nil {
 					return err
 				}
 
+				// convert bitrate found to kilobits per second
 				bitrateKilobitsS, _ := strconv.Atoi(strings.TrimSpace(string(output)))
 				bitrateKilobitsS /= 1000
 
+				// ignore file if bitrate is zero and options set to ignore zero values
 				if bitrateKilobitsS == 0 && ignoreZero {
 					return nil
 				}
 
+				// four different conditions when output should be added
+				// 1. when both min and max are set to be ignored
+				// 2. when min is set then only print if greater than min
+				// 3. when max is set then only print if less than max
+				// 4. when both are set then only print if within min and max
 				if (minB == 0 && maxB == 0) ||
 					(maxB == 0 && minB != 0 && bitrateKilobitsS >= minB) ||
 					(minB == 0 && maxB != 0 && bitrateKilobitsS <= maxB) ||
 					(minB != 0 && maxB != 0 && bitrateKilobitsS >= minB && bitrateKilobitsS <= maxB) {
+					// format output and append new row to top of output box
 					outputText = fmt.Sprintf("%dKb/s %s\n", bitrateKilobitsS, path) + outputText
 					outputBox.SetText(outputText)
 				}
@@ -87,18 +115,21 @@ func main() {
 				return nil
 			})
 
+			// handle error if unable to walk directory
 			if err != nil {
 				panic(err)
 			}
 		}()
 	})
 
+	// create path entry field and folder selection button layout
 	entryLayout := container.New(
 		layout.NewHBoxLayout(),
 		path,
 		folderSelect,
 	)
 
+	// create main window layout
 	mainWindow.SetContent(container.NewVBox(
 		entryLayout,
 		options,
@@ -106,6 +137,7 @@ func main() {
 		outputBox,
 	))
 
+	// run main window
 	mainWindow.SetMaster()
 	mainWindow.Resize(fyne.NewSize(960, 610))
 	mainWindow.Show()
--- options.go
+++ options.go
@@ -10,39 +10,46 @@ import (
 )
 
 func showOptions(app fyne.App) {
+	// create new window from main app
 	optionsWindow := app.NewWindow("Options")
 
+	// create max bitrate widget and set validator to only allow numbers
 	maxBitrate := widget.NewEntry()
 	maxBitrate.SetText(strconv.Itoa(maxB))
 	maxBitrate.Validator = validation.NewRegexp(`^[0-9]*$`, "Please enter a valid number")
 
+	// create min bitrate widget and set validator to only allow numbers
 	minBitrate := widget.NewEntry()
 	minBitrate.SetText(strconv.Itoa(minB))
 	minBitrate.Validator = validation.NewRegexp(`^[0-9]*$`, "Please enter a valid number")
 
-	exclude0 := widget.NewCheck("", func(b bool) {})
-	exclude0.SetChecked(true)
+	// create checkbox to exclude bitrates of zero
+	excludeZero := widget.NewCheck("", func(b bool) {})
+	excludeZero.SetChecked(true)
 
+	// create form layout and set relevant values on submit
 	options := &widget.Form{
 		Items: []*widget.FormItem{
 			{Text: "Max bitrate (Kb/s)", Widget: maxBitrate},
 			{Text: "Min bitrate (Kb/s)", Widget: minBitrate},
-			{Text: "Exclude 0Kb/s", Widget: exclude0},
+			{Text: "Exclude 0Kb/s", Widget: excludeZero},
 		},
 		OnSubmit: func() {
 			maxB, _ = strconv.Atoi(maxBitrate.Text)
 			minB, _ = strconv.Atoi(minBitrate.Text)
-			ignoreZero = exclude0.Checked
+			ignoreZero = excludeZero.Checked
 
 			optionsWindow.Close()
 		},
 	}
 
+	// create main layout with additional information label
 	content := container.NewVBox(
 		widget.NewLabelWithStyle("Enter 0 to remove limit", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
 		options,
 	)
 
+	// run window
 	optionsWindow.SetContent(content)
 	optionsWindow.Resize(fyne.NewSize(400, 200))
 	optionsWindow.SetFixedSize(true)
