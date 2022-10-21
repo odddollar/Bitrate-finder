017428211c5b6e3ddc90efd4b2a2d683f98ef311
Simon Eason <sie.eason@gmail.com>
2022-10-21 12:05:14 +0800

    Moved everything to call init() and separate callbacks

--- main.go
+++ main.go
@@ -37,7 +37,8 @@ var outputBox *widget.Entry
 var exportCSV *widget.Button
 var progress *widget.ProgressBar
 
-func main() {
+// initialise main ui widgets
+func init() {
 	// create app
 	a = app.New()
 	mainWindow = a.NewWindow("Bitrate Finder")
@@ -57,150 +58,19 @@ func main() {
 	path.SetPlaceHolder("Path to videos")
 
 	// create folder selection button widget
-	folderSelect = widget.NewButton("...", func() {
-		// create dialog with callback
-		d := dialog.NewFolderOpen(func(lu fyne.ListableURI, err error) {
-			// check if nothing has been selected
-			if lu == nil {
-				return
-			}
-
-			// get selected items in directory and remove filename from first item
-			URIList, _ := lu.List()
-			j := strings.Split(URIList[0].Path(), "/")
-			formattedText := strings.Join(j[0:len(j)-1], "/")
-
-			// set formatted text in folder field
-			path.SetText(formattedText)
-
-			// set progress bar maximum value
-			go func() {
-				progress.Max = float64(getNumFiles(formattedText))
-			}()
-		}, mainWindow)
-
-		// show folder selection dialog
-		d.Show()
-	})
+	folderSelect = widget.NewButton("...", folderCallback)
 
 	// create options button to open options window
-	options = widget.NewButton("Options", func() {
-		showOptions(a)
-	})
+	options = widget.NewButton("Options", optionsCallback)
 
 	// create output box with minimum number of rows visible
 	outputBox = widget.NewMultiLineEntry()
 
 	// button that exports output box content to csv
-	exportCSV = widget.NewButton("Export to CSV", func() {
-		// don't do anything if nothing in output box
-		if outputText == "" {
-			return
-		}
-
-		go func() {
-			// split string into array and remove first and last indexes
-			outputLines := strings.Split(outputText, "\n")
-			outputLines = outputLines[1 : len(outputLines)-1]
-
-			// create array of formatted csv values
-			var outCSV []string
-			for i := 0; i < len(outputLines); i++ {
-				t := strings.SplitN(outputLines[i], " ", 2)
-				t[0] = strings.Trim(t[0], "Kb/s")
-				outCSV = append(outCSV, strings.Join(t, ","))
-			}
-
-			// open save window
-			d := dialog.NewFileSave(func(uc fyne.URIWriteCloser, err error) {
-				// prevent crashing if nothing was selected
-				if uc != nil {
-					return
-				}
-
-				path := uc.URI().Path()
-				writeArrayToFile(path, outCSV)
-			}, mainWindow)
-			d.Show()
-		}()
-	})
+	exportCSV = widget.NewButton("Export to CSV", exportCallback)
 
 	// create run button widget
-	run = widget.NewButton("Run", func() {
-		// don't do anything if no path entered
-		if path.Text == "" {
-			return
-		}
-
-		// run as separate thread
-		go func() {
-			// disable button to prevent re-running and exporting until complete
-			run.Disable()
-			exportCSV.Disable()
-
-			// walk through selected directory
-			err := filepath.Walk(path.Text, func(path string, info fs.FileInfo, err error) error {
-				// return errors that occur
-				if err != nil {
-					return err
-				}
-
-				// only run code for files
-				if info.IsDir() {
-					return nil
-				}
-
-				// execute command (without cmd window) to find bitrate and handle error
-				command := exec.Command("cmd", "/c", "ffprobe", "-v", "error", "-show_entries", "format=bit_rate", "-of", "default=noprint_wrappers=1:nokey=1", path)
-				command.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
-				output, err := command.Output()
-				if err != nil {
-					return err
-				}
-
-				// increase progress bar
-				progress.SetValue(progress.Value + 1)
-
-				// convert bitrate found to kilobits per second
-				bitrateKilobitsS, _ := strconv.Atoi(strings.TrimSpace(string(output)))
-				bitrateKilobitsS /= 1000
-
-				// ignore file if bitrate is zero and options set to ignore zero values
-				if bitrateKilobitsS == 0 && ignoreZero {
-					return nil
-				}
-
-				// four different conditions when output should be added
-				// 1. when both min and max are set to be ignored
-				// 2. when min is set then only print if greater than min
-				// 3. when max is set then only print if less than max
-				// 4. when both are set then only print if within min and max
-				if (minB == 0 && maxB == 0) ||
-					(maxB == 0 && minB != 0 && bitrateKilobitsS >= minB) ||
-					(minB == 0 && maxB != 0 && bitrateKilobitsS <= maxB) ||
-					(minB != 0 && maxB != 0 && bitrateKilobitsS >= minB && bitrateKilobitsS <= maxB) {
-					// format output and append new row to top of output box
-					outputText = fmt.Sprintf("%dKb/s %s\n", bitrateKilobitsS, path) + outputText
-					outputBox.SetText(outputText)
-				}
-
-				return nil
-			})
-
-			// handle error if unable to walk directory
-			if err != nil {
-				panic(err)
-			}
-
-			// add completion message
-			outputText = "Complete\n" + outputText
-			outputBox.SetText(outputText)
-
-			// re-enable buttons
-			run.Enable()
-			exportCSV.Enable()
-		}()
-	})
+	run = widget.NewButton("Run", runCallback)
 	run.Importance = widget.HighImportance
 
 	// create path entry field and folder selection button layout
@@ -229,19 +99,163 @@ func main() {
 		progress,
 	)
 
-	// create main window layout
-	mainWindow.SetContent(container.NewBorder(
+	// main content hierarchy
+	content := container.NewBorder(
 		topLayout,
 		bottomLayout,
 		nil,
 		nil,
 		outputBox,
-	))
+	)
 
-	// run main window
+	// set main window properties
+	mainWindow.SetContent(content)
 	mainWindow.SetMaster()
 	mainWindow.SetIcon(resourceIconPng)
 	mainWindow.Resize(fyne.NewSize(960, 610))
+}
+
+func folderCallback() {
+	// create dialog with callback
+	d := dialog.NewFolderOpen(func(lu fyne.ListableURI, err error) {
+		// check if nothing has been selected
+		if lu == nil {
+			return
+		}
+
+		// get selected items in directory and remove filename from first item
+		URIList, _ := lu.List()
+		j := strings.Split(URIList[0].Path(), "/")
+		formattedText := strings.Join(j[0:len(j)-1], "/")
+
+		// set formatted text in folder field
+		path.SetText(formattedText)
+
+		// set progress bar maximum value
+		go func() {
+			progress.Max = float64(getNumFiles(formattedText))
+		}()
+	}, mainWindow)
+
+	// show folder selection dialog
+	d.Show()
+}
+
+func optionsCallback() {
+	showOptions(a)
+}
+
+func exportCallback() {
+	// don't do anything if nothing in output box
+	if outputText == "" {
+		return
+	}
+
+	go func() {
+		// split string into array and remove first and last indexes
+		outputLines := strings.Split(outputText, "\n")
+		outputLines = outputLines[1 : len(outputLines)-1]
+
+		// create array of formatted csv values
+		var outCSV []string
+		for i := 0; i < len(outputLines); i++ {
+			t := strings.SplitN(outputLines[i], " ", 2)
+			t[0] = strings.Trim(t[0], "Kb/s")
+			outCSV = append(outCSV, strings.Join(t, ","))
+		}
+
+		// open save window
+		d := dialog.NewFileSave(func(uc fyne.URIWriteCloser, err error) {
+			// prevent crashing if nothing was selected
+			if uc != nil {
+				return
+			}
+
+			path := uc.URI().Path()
+			writeArrayToFile(path, outCSV)
+		}, mainWindow)
+		d.Show()
+	}()
+}
+
+func runCallback() {
+	// don't do anything if no path entered
+	if path.Text == "" {
+		return
+	}
+
+	// run as separate thread
+	go func() {
+		// disable button to prevent re-running and exporting until complete
+		run.Disable()
+		exportCSV.Disable()
+
+		// walk through selected directory
+		err := filepath.Walk(path.Text, func(path string, info fs.FileInfo, err error) error {
+			// return errors that occur
+			if err != nil {
+				return err
+			}
+
+			// only run code for files
+			if info.IsDir() {
+				return nil
+			}
+
+			// execute command (without cmd window) to find bitrate and handle error
+			command := exec.Command("cmd", "/c", "ffprobe", "-v", "error", "-show_entries", "format=bit_rate", "-of", "default=noprint_wrappers=1:nokey=1", path)
+			command.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+			output, err := command.Output()
+			if err != nil {
+				return err
+			}
+
+			// increase progress bar
+			progress.SetValue(progress.Value + 1)
+
+			// convert bitrate found to kilobits per second
+			bitrateKilobitsS, _ := strconv.Atoi(strings.TrimSpace(string(output)))
+			bitrateKilobitsS /= 1000
+
+			// ignore file if bitrate is zero and options set to ignore zero values
+			if bitrateKilobitsS == 0 && ignoreZero {
+				return nil
+			}
+
+			// four different conditions when output should be added
+			// 1. when both min and max are set to be ignored
+			// 2. when min is set then only print if greater than min
+			// 3. when max is set then only print if less than max
+			// 4. when both are set then only print if within min and max
+			if (minB == 0 && maxB == 0) ||
+				(maxB == 0 && minB != 0 && bitrateKilobitsS >= minB) ||
+				(minB == 0 && maxB != 0 && bitrateKilobitsS <= maxB) ||
+				(minB != 0 && maxB != 0 && bitrateKilobitsS >= minB && bitrateKilobitsS <= maxB) {
+				// format output and append new row to top of output box
+				outputText = fmt.Sprintf("%dKb/s %s\n", bitrateKilobitsS, path) + outputText
+				outputBox.SetText(outputText)
+			}
+
+			return nil
+		})
+
+		// handle error if unable to walk directory
+		if err != nil {
+			panic(err)
+		}
+
+		// add completion message
+		outputText = "Complete\n" + outputText
+		outputBox.SetText(outputText)
+
+		// re-enable buttons
+		run.Enable()
+		exportCSV.Enable()
+	}()
+}
+
+func main() {
+	// run app
 	mainWindow.Show()
 	a.Run()
 }
