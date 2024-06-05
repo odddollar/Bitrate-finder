c46fcd40ae2f1ca47a6fe5b0731408513b878f6b
Simon Eason <sie.eason@gmail.com>
2024-06-05 14:26:26 +0800

    Removed another unnecessary thread

--- background/run.go
+++ background/run.go
@@ -21,81 +21,78 @@ func RunCallback() {
 		global.Progress.SetValue(0)
 	}
 
-	// run as separate thread
-	go func() {
-		// disable all buttons when running
-		global.FolderSelect.Disable()
-		global.Opt.Disable()
-		global.Run.Disable()
-		global.ExportCSV.Disable()
+	// disable all buttons when running
+	global.FolderSelect.Disable()
+	global.Opt.Disable()
+	global.Run.Disable()
+	global.ExportCSV.Disable()
 
-		// set progress bar maximum value
-		global.Progress.Max = float64(getNumFiles(global.Path.Text))
+	// set progress bar maximum value
+	global.Progress.Max = float64(getNumFiles(global.Path.Text))
 
-		// split list of whitelisted file extensions
-		whitelistedExtensionsSplit := strings.Split(global.WhitelistedExtensions, ",")
+	// split list of whitelisted file extensions
+	whitelistedExtensionsSplit := strings.Split(global.WhitelistedExtensions, ",")
 
-		// walk through selected directory
-		err := filepath.Walk(global.Path.Text, func(path string, info fs.FileInfo, err error) error {
-			// return errors that occur
-			if err != nil {
-				return err
-			}
-
-			// only run code for files
-			if info.IsDir() {
-				return nil
-			}
-
-			// increase progress bar
-			global.Progress.SetValue(global.Progress.Value + 1)
-
-			// if current path's extension isn't in the whitelist then ignore
-			ext := strings.TrimPrefix(filepath.Ext(path), ".")
-			if !slices.Contains(whitelistedExtensionsSplit, ext) && whitelistedExtensionsSplit[0] != "" {
-				return nil
-			}
-
-			// get bitrate in kilobits/s
-			bitrateKilobitsS := getBitrate(path)
-
-			// ignore file if bitrate is zero and options set to ignore zero values
-			if bitrateKilobitsS == 0 && global.IgnoreZero {
-				return nil
-			}
-
-			// four different conditions when output should be added
-			// 1. when both min and max are set to be ignored
-			// 2. when min is set then only print if greater than min
-			// 3. when max is set then only print if less than max
-			// 4. when both are set then only print if within min and max
-			if (global.MinB == 0 && global.MaxB == 0) ||
-				(global.MaxB == 0 && global.MinB != 0 && bitrateKilobitsS >= global.MinB) ||
-				(global.MinB == 0 && global.MaxB != 0 && bitrateKilobitsS <= global.MaxB) ||
-				(global.MinB != 0 && global.MaxB != 0 && bitrateKilobitsS >= global.MinB && bitrateKilobitsS <= global.MaxB) {
-				// format output and append new row to top of output box
-				t, _ := global.OutputText.Get()
-				global.OutputText.Set(fmt.Sprintf("%dKb/s %s\n", bitrateKilobitsS, path) + t)
-			}
-
-			return nil
-		})
-
-		// handle error if unable to walk directory
+	// walk through selected directory
+	err := filepath.Walk(global.Path.Text, func(path string, info fs.FileInfo, err error) error {
+		// return errors that occur
 		if err != nil {
-			global.ErrorDialog(err)
+			return err
 		}
 
-		// add completion message
-		t, _ := global.OutputText.Get()
-		global.OutputText.Set("Complete\n" + t)
+		// only run code for files
+		if info.IsDir() {
+			return nil
+		}
 
-		// re-enable buttons
-		global.FolderSelect.Enable()
-		global.Opt.Enable()
-		global.Run.Enable()
-		global.ExportCSV.Enable()
-	}()
+		// increase progress bar
+		global.Progress.SetValue(global.Progress.Value + 1)
+
+		// if current path's extension isn't in the whitelist then ignore
+		ext := strings.TrimPrefix(filepath.Ext(path), ".")
+		if !slices.Contains(whitelistedExtensionsSplit, ext) && whitelistedExtensionsSplit[0] != "" {
+			return nil
+		}
+
+		// get bitrate in kilobits/s
+		bitrateKilobitsS := getBitrate(path)
+
+		// ignore file if bitrate is zero and options set to ignore zero values
+		if bitrateKilobitsS == 0 && global.IgnoreZero {
+			return nil
+		}
+
+		// four different conditions when output should be added
+		// 1. when both min and max are set to be ignored
+		// 2. when min is set then only print if greater than min
+		// 3. when max is set then only print if less than max
+		// 4. when both are set then only print if within min and max
+		if (global.MinB == 0 && global.MaxB == 0) ||
+			(global.MaxB == 0 && global.MinB != 0 && bitrateKilobitsS >= global.MinB) ||
+			(global.MinB == 0 && global.MaxB != 0 && bitrateKilobitsS <= global.MaxB) ||
+			(global.MinB != 0 && global.MaxB != 0 && bitrateKilobitsS >= global.MinB && bitrateKilobitsS <= global.MaxB) {
+			// format output and append new row to top of output box
+			t, _ := global.OutputText.Get()
+			global.OutputText.Set(fmt.Sprintf("%dKb/s %s\n", bitrateKilobitsS, path) + t)
+		}
+
+		return nil
+	})
+
+	// handle error if unable to walk directory
+	if err != nil {
+		global.ErrorDialog(err)
+	}
+
+	// add completion message
+	t, _ := global.OutputText.Get()
+	global.OutputText.Set("Complete\n" + t)
+
+	// re-enable buttons
+	global.FolderSelect.Enable()
+	global.Opt.Enable()
+	global.Run.Enable()
+	global.ExportCSV.Enable()
 }
 
 func getBitrate(path string) int {
